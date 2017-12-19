56c5337647a1a325674eae0b4c47b416647f1006
astrofaulkner <astrophysman2010@yahoo.com>
2017-12-19 16:10:00 -0500

    Added correlation plots and histograms.

--- src/muck_with_Data.py
+++ src/muck_with_Data.py
@@ -2,6 +2,8 @@ import pylab as pyl
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sb
+import math
 
 def run_test():
     
@@ -15,19 +17,50 @@ def run_test():
     #print(df.head())
 
     # Create histograms
-    df['result'].value_counts().plot(kind='bar', title='Results')
+    #df['result'].value_counts().plot(kind='bar', title='Results')
 
+    # Create 'ext' column for file extensions
     df['ext'] = 0
     for i in range(len(df.index)):
         df['ext'][i] = df['name'][i].split('.')[1]
     #print(df.head())
 
+    # Plot cross table of various extensions vs results
     df2 = df.loc[df['ext'].isin(['js', 'pcl'])]
     pclass_xt = pd.crosstab(df2['ext'], df2['result'])
     pclass_xt_pct = pclass_xt.div(pclass_xt.sum(1).astype(float), axis=0)
-    pclass_xt_pct.plot(kind='bar', stacked=True, title='Extension vs. Result')
-    #pyl.xlabel('Extension')
-    #pyl.ylabel('Result')
+    #pclass_xt_pct.plot(kind='bar', stacked=True, title='Extension vs. Result')
+
+    # Generate a mapping between file extensions and integers
+    exts = sorted(df['ext'].unique())
+    exts_map = dict(zip(exts, range(0, len(exts) + 1)))
+    df['ext_val'] = df['ext'].map(exts_map).astype(int)
+    #print(df.head())
+
+    # Plot the ext_val histogram
+    #df['ext_val'].hist()
+    #pyl.title('Ext. Mapping Histogram')
+
+    # Plot cross table of all extensions vs result
+    pclass_xt = pd.crosstab(df['result'], df['ext_val'])
+    pclass_xt_pct = pclass_xt.div(pclass_xt.sum(1).astype(float), axis=0)
+    #pclass_xt_pct.plot(kind='bar', stacked=True, title='Mapped Extension vs. Result')
+
+    # Plot joint plot between ext_val and len(filename)
+    df['FN_len'] = 0
+    for i in range(len(df.index)):
+        df['FN_len'][i] = len(df['name'][i])-len(df['ext'][i])
+    #sb.jointplot(data=df, x='ext_val', y='FN_len', kind='reg', color='g')
+
+    # Create pivot table of result vs ext for mean filename length
+    df['ext_val_trumpd'] = 0
+    for i in range(len(df.index)):
+        df['ext_val_trumpd'][i] = df['ext_val'][i] % 10.
+    df3 = df.pivot_table(index='result', columns='ext_val_trumpd', values='FN_len', aggfunc=np.median)
+    #sb.heatmap(df3, annot=True, fmt=".1f")
+
+    # Create Pearson's correlation coefficient matrix for dataframe
+    sb.heatmap(df.corr(), annot=True, fmt=".2f")
 
     pyl.show()
 
